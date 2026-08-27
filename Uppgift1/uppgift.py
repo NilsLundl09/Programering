566ed324b5c5006014455534ec7217a9bd3cdcaa
NilsLundl09 <nils.lundell@icloud.com>
2026-08-27 11:26:25 +0200

    Modul 1 klar

---
+++ Uppgift1/uppgift.py
@@ -0,0 +1,19 @@
+print("hello world")
+print("""
+Hello world
+Hello world
+Hello world
+Hello world
+Hello world
+Hello world
+Hello world
+Hello world
+Hello world
+Hello world
+""")
+print("Tung tung tung sahur \n tung tung tung mator")
+print("Nils" + "Lundell")
+Age = input("Skriv din ålder här:")
+First_Name = input("Skriv ditt namn här:")
+Last_Name = input("skriv ditt efternamn här:")
+print("Hej" + " " + First_Name + " " + Last_Name + "," + Age)
